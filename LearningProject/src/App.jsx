4bc59aac226f07e85551e59b7fd76f2fab5d57d8
rajesh <ajoypal.raj@gmail.com>
2025-02-05 21:29:29 +0600

    c4: Why to use State - Part 3: useState Hook

--- LearningProject/src/App.jsx
+++ LearningProject/src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
+
 function App() {
-  let userName = "Rajesh"
+  // Akhon dekhbo problem tar solutio. Er jonno amader hook use korte hobe.
+  // hook hocce special type of function. Jgula state management a help kore. dorun apner akta variable ace 'a' jar value initially 1. Apni chaccen a er value update kore 2 korer jonno. ai j a er value 1 teke 2 hobe ata kei bole a er state change ba update howa. Toh ai state management er jonno amra muloto react hook use kori.
+  // aikhane amra useState hook er use dekhbo.
+  
+  const [userName, setUserName] = useState("Rajesh");
+
   const changeUserName = ()=>{
-    // akhon amra userName print kore dekhi:
-    console.log("before click : ", userName);
-    userName = "Rajesh Pal";
-    console.log("After click : ", userName);
-    //ata run korale dekhte pabe userName tik e change hocce but UI a sheta update hoi na variable use korle. Er karon hocce amra react use korci. Mane amra akhon directly DOM er sathe interect korte parbo na er jonno amader React k bolte hobe kothai ki korte hobe. na hole sha kaj ta korbe na.
+    setUserName("Rajesh Pal");
   }
+  // akhon button click korle dekhbe shob amader expectation onujayi kaj korce.
 
   return (
     <>
