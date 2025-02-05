b63a09b2f5e28222e731ee91f13a84759da0a6ab
rajesh <ajoypal.raj@gmail.com>
2025-02-05 20:42:32 +0600

    c2: jsx,passing multiple element,creating variable and use of variables

--- LearningProject/src/App.jsx
+++ LearningProject/src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return "Hello Rajesh";
+  const j = 200;
+  return (
+    <>
+      <h1> Hello Rajesh {j}</h1>
+      <h2>how to pass multiple element from a single return</h2>
+    </>
+  )
 }
 
 export default App
