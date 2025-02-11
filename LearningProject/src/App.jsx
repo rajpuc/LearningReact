82e9fa848a1ab46a63de6f7f66455721ceed5093
rajesh <ajoypal.raj@gmail.com>
2025-02-11 12:38:34 +0600

    c10: Simple website example using react and also showed how to reuse components

--- LearningProject/src/App.jsx
+++ LearningProject/src/App.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
-
+import Footer from "./components/Footer";
+import CardOne from "./components/cards/CardOne";
+import CardTwo from "./components/cards/CardTwo";
+import CardThree from "./components/cards/CardThree";
 // Components
-// Component file er name shob shomoy Upper case letter diye suru hobe.   
+// Component file er name shob shomoy Upper case letter diye suru hobe.
 
 const App = () => {
   return (
     <>
-    <Header/>
-    <Header/>
-    <Header/>
-
+      <Header />
+        <div className="flex items-center justify-between px-2">
+          <CardOne />
+          <CardOne />
+          <CardOne />
+        </div >
+        <div className="flex items-center justify-between px-2">
+          <CardTwo />
+          <CardTwo />
+          <CardTwo />
+        </div>
+        <div className="flex items-center justify-between px-2">
+          <CardThree />
+          <CardThree />
+          <CardThree />
+        </div>
+      <Footer />
     </>
   );
 };
---
+++ LearningProject/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Footer = () =>{
+    return (
+        <div className="flex bg-emerald-100 w-full items-center justify-center  text-xl py-2">
+            This is footer.
+        </div>
+    );
+}
+
+export default Footer;
