f4e39e0cfad36937655ba00b194cde4a1684ab2a
rajesh <ajoypal.raj@gmail.com>
2025-02-11 17:42:00 +0600

    c11: Props with JSON

--- LearningProject/src/App.jsx
+++ LearningProject/src/App.jsx
@@ -1,22 +1,60 @@
 import React, { useState } from "react";
 import Card from "./components/Card";
-import AnotherCard from "./components/AnotherCard";
 
-
-// Props 
+// Props with JSON Array
 
 const App = () => {
-  const passedProperty = "Hello There";
+  const users = [
+    {
+      name: "Shah Rukh Khan",
+      age: 58,
+      city: "Mumbai",
+      profession: "Actor",
+      image: "https://picsum.photos/id/122/400/300",
+    },
+    {
+      name: "Deepika Padukone",
+      age: 28,
+      city: "Mumbai",
+      profession: "Actress",
+      image: "https://picsum.photos/id/232/400/300",
+    },
+    {
+      name: "Virat Kohli",
+      age: 35,
+      city: "Delhi",
+      profession: "Cricketer",
+      image: "https://picsum.photos/id/323/400/300",
+    },
+    {
+      name: "Priyanka Chopra",
+      age: 43,
+      city: "Mumbai",
+      profession: "Actress, Singer",
+      image: "https://picsum.photos/id/433/400/300",
+    },
+    {
+      name: "Ranbir Kapoor",
+      age: 41,
+      city: "Mumbai",
+      profession: "Actor",
+      image: "https://picsum.photos/id/253/400/300",
+    },
+  ];
   return (
-    <>
-      {/* 1st way of passing a property */}
-      <Card passedProperty={passedProperty}/>
-      {/* 2nd way of passing a property */}
-      <Card passedProperty="you can also use this syntax"/>
-      {/* Another way of Recieve a property. Please Go to AnotherCard.jsx to learn: */}
-      <AnotherCard username="Rajesh Pal" city="Rangamati" age="25"/>
-
-    </>
+    <div className="flex flex-wrap justify-between gap-5">
+      {users.map((item, index) => {
+        
+          return (<Card key={index}
+            name={item.name}
+            age={item.age}
+            city={item.city}
+            profession={item.profession}
+            src={item.image}
+          />);
+      
+      })}
+    </div>
   );
 };
 
