037cf805ca6ec29e920984ec47efba7481c6c7d8
rajesh <ajoypal.raj@gmail.com>
2025-02-11 10:18:49 +0600

    c6: tailwind css added

--- LearningProject/src/App.jsx
+++ LearningProject/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <h3>Simple Number Increment Decrement Project:</h3>
-      <h1> Count {counter}</h1>
+      <h1 className="bg-green-700 ml-7"> Count {counter}</h1>
       <button onClick={()=>{setCounter( counter+1 )}}>Increment Counter</button>
       <button onClick={()=>{setCounter( counter-1 )}}>Decrement Counter</button>
     </>
