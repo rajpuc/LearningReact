f4c3c1cfdc9f9a3a50d03b4176e25cc4bdedd57e
rajesh <ajoypal.raj@gmail.com>
2025-02-11 12:18:16 +0600

    c9: Components

--- LearningProject/src/App.jsx
+++ LearningProject/src/App.jsx
@@ -1,31 +1,17 @@
 import React, { useState } from "react";
+import Header from "./components/Header";
 
-// 2 way binding with form submi example
+// Components
+// Component file er name shob shomoy Upper case letter diye suru hobe.   
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const submitHandeler = (e) => {
-    e.preventDefault();
-    console.log(username);
-    setUsername("");
-  };
   return (
-    <div>
-      <form onSubmit={submitHandeler} className="w-full mt-9">
-        <div className="flex items-center mx-auto gap-2 w-fit">
-          <input
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value);
-              console.log(username);
-            }}
-            className="border px-2"
-            type="text"
-          />
-          <button className="bg-blue-300 px-2 py-1/2 rounded">Submit</button>
-        </div>
-      </form>
-    </div>
+    <>
+    <Header/>
+    <Header/>
+    <Header/>
+
+    </>
   );
 };
 
