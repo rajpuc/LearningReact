f4fb1673c3a351ad0006f3facc8a0aec386b4724
rajesh <ajoypal.raj@gmail.com>
2025-02-11 11:37:28 +0600

    c8: Two way binding

--- LearningProject/src/App.jsx
+++ LearningProject/src/App.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-// form handeling
+// 2 way binding
 
 const App = () => {
-  const submitHandeler = (e) => {
-    e.preventDefault();
-    console.log("Rajesh Pal");
-  };
+  const [username, setUsername] = useState("");
   return (
     <div>
-      <form
-        onSubmit={
-          submitHandeler
-        }
-        className="w-full mt-9"
-      >
+      <form className="w-full mt-9">
         <div className="flex items-center mx-auto gap-2 w-fit">
-          <input className="border px-2" type="text" name="" id="" />
+          <input
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+              console.log(username);
+            }}
+            className="border px-2"
+            type="text"
+          />
           <button className="bg-blue-300 px-2 py-1/2 rounded">Submit</button>
         </div>
       </form>
